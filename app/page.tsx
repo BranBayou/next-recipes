4080b984694a2493cbeda7c0fe6a7bb21069a6ee
Berhanu Baye <berhan.baye@gmail.com>
2024-05-26 09:17:30 +0300

    Add shadcn ui for cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,52 @@
 import { Badge } from "@/components/ui/badge";
-import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 
-export default function Home() {
+export default function HomePage() {
+  const cuisines: Array<String> = [
+    "All",
+    "American",
+    "Asian",
+    "Chinese",
+    "Greek",
+    "Indian",
+    "Italian",
+    "Japanese",
+    "Korean",
+    "Mexican",
+  ];
+
+  const recipes = [
+    {
+      id: 1,
+      title: "Recipe 1",
+      cuisine: "American",
+    },
+    {
+      id: 2,
+      title: "Sandwitch",
+      cuisine: "Italian",
+    }
+  ]
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>Hello there!</Button>
-    </main>
+    <div>
+      {cuisines.map((cuisine, idx) => {
+        return <Badge key={`${cuisine}-${idx}`}>{cuisine}</Badge>;
+      })}
+
+      {recipes.map((recipe, idx) => 
+      <Card key={`${recipe.id}-${idx}`}>
+        <CardHeader>
+          {/* img */}
+        </CardHeader>
+        <CardContent>
+          <p>{recipe.title}</p>
+        </CardContent>
+        <CardFooter>
+          <p>{recipe.cuisine}</p>
+        </CardFooter>
+      </Card>
+      )}
+    </div>
   );
 }
